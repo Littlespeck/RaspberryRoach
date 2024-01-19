be2c6180ef79a9267ce021c3098be1a2800bd6f1
Littlespeck <154754782+Littlespeck@users.noreply.github.com>
2024-01-18 23:21:18 -0500

    Starting to create Game_Scene
    Combined player pieces. Cleaned up some player code
    
    created temporary scenes to be replaced by others later (namely main menu)

--- Resources/Scenes/Control/scene_handler.gd
+++ Resources/Scenes/Control/scene_handler.gd
@@ -3,5 +3,14 @@ extends Node
 func _ready():
 	load_main_menu()
 
-func load_main_menu():
+func on_new_game_pressed():
+	get_node("MainMenu").queue_free()
+	var game_scene = load("res://Resources/Scenes/Control/game_scene.tscn").instantiate()
+	add_child(game_scene)
+	game_scene.connect("game_finished", unload_game)
+
+func unload_game():
 	pass
+
+func load_main_menu():
+	get_node("MainMenu/M/NewGame").connect("pressed", on_new_game_pressed)
--- Resources/Scripts/game_scene.gd
+++ Resources/Scripts/game_scene.gd
@@ -2,8 +2,15 @@ extends Node2D
 
 signal game_finished(result)
 
-var map_node
+var jump_mode = false
+var player_character
 
 func _ready():
-	map_node = load("res://Resources/Scenes/Maps/map_1.tscn").instantiate
-	add_child(map_node)
+	var new_map = load("res://Resources/Scenes/Maps/map_1.tscn").instantiate()
+	add_child(new_map)
+	var start_platform = load("res://Resources/Scenes/Objects/bacon_platform.tscn").instantiate()
+	start_platform.position = Vector2(360, 1000)
+	add_child(start_platform)
+	player_character = load("res://Resources/Scenes/Objects/PlayerNode.tscn").instantiate()
+	player_character.position = Vector2(360, 900)
+	add_child(player_character)
