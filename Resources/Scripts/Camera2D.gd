a5eb559ecdb3f70ecaace4bf2de9b5ff34088015
Littlespeck <154754782+Littlespeck@users.noreply.github.com>
2024-01-19 20:25:31 -0500

    Camera Offset
    Camera follow player up and not down

--- Resources/Scripts/Camera2D.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-
-
-func _process(delta):
-	if get_parent().player_character.get_node("Roach PC").velocity.y < 0:
-		position.y = get_parent().player_character.get_node("Roach PC").position.y
--- Resources/Scripts/game_scene.gd
+++ Resources/Scripts/game_scene.gd
@@ -4,6 +4,9 @@ signal game_finished(result)
 
 var jump_mode = false
 var player_character
+var screen_size = 2080.0
+var camera_altitude = screen_size / 2.0
+var camera_offset = 600
 
 func _ready():
 	var new_map = load("res://Resources/Scenes/Maps/map_1.tscn").instantiate()
@@ -14,3 +17,9 @@ func _ready():
 	player_character = load("res://Resources/Scenes/Objects/PlayerNode.tscn").instantiate()
 	player_character.position = Vector2(360, 900)
 	add_child(player_character)
+	get_node("Camera2D").position.y = camera_altitude
+
+func _process(delta):
+	if get_node("Player/Roach PC").position.y + camera_offset < camera_altitude:
+		camera_altitude = get_node("Player/Roach PC").position.y + camera_offset
+		get_node("Camera2D").position.y = camera_altitude
