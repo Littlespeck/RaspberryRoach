cb3028bf246d968306acb329fdc38fac02f03fe0
Littlespeck <154754782+Littlespeck@users.noreply.github.com>
2024-01-26 19:12:59 -0500

    knife big

--- Resources/Scripts/gummy.gd
+++ Resources/Scripts/gummy.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 
 func _on_body_entered(_body):
-	get_parent().get_parent().platform_spread = get_parent().get_parent().platform_spread * .5
+	get_parent().get_parent().platform_spread = get_parent().get_parent().platform_spread * .8
 	get_parent().get_parent().Pup_Points += 200
 	queue_free()
